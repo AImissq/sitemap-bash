cf2001c028495bc58547ad0b0f1009763b8b044a
Tom Daley <tjd@powerdaley.com>
2019-04-14 16:35:44 -0500

    revisions for generalization

--- sitemapmaker.sh
+++ sitemapmaker.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-url=https://www.powerdaley.com
+
+#----------
+# I copied this script from www.lostsaloon.com/technology/how-to-create-an-xml-sitemap-using-wget-and-shell-script/
+#
+# It is the brainchild of "barkeep", who posted it August 8,2015.
+# I copied and ran the script pretty-much as-is April 14, 2019 and it worked perfectly.
+#----------
+
+if [ "$1" != "" ]; then
+    echo "Crawling " $1
+else
+    echo "usage: ./sitemapmaker URL"
+    echo "     where URL is the full URL to the web site you want to create a map for.
+    echo "example: $ ./sitemapmaker https://www.attorney.bot"
+    exit 1
+fi
+
+url=$1
 
 wget --spider --recursive --level=inf --no-verbose --output-file=./links.txt $url
 grep -i URL links.txt | awk -F 'URL:' '{print $2}' | awk '{$1=$1};1' | awk '{print $1}' | sort -u | sed '/^$/d' > sorted_links.txt
@@ -23,3 +40,11 @@ while read p; do
 esac
 done < sorted_links.txt
 echo "</urlset>" >> sitemap.xml
+
+# Cleanup
+rm ./links.txt
+rm ./sorted_links.txt
+
+# Announce completion
+echo "Site map created as sitemap.xml"
+exit 0
